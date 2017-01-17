d289d9939c1dfeffe24b012b1eeeedf34d17ef04
WAAutoMaton <a547673135@live.cn>
2017-01-17 10:09:39 +0800

    删除无效文件

--- is_accepted.py
+++
@@ -1,9 +0,0 @@
-import network,re;
-
-def IsAccepted(username,problemID):
-    page=network.getpage("http://www.lydsy.com/JudgeOnline/userinfo.php?user=%s" % username)
-    pattern=re.compile("p\\(%d\\)(.*)</script>" % problemID)
-    return re.search(pattern,page)
-
-
-
